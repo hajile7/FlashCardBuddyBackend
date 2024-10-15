9df1d53ffb6ba7de3fbefa898e27f56b57ac2aa0
Dloganreeves <loganreeves1@yahoo.com>
2024-10-15 14:35:05 -0400

    Flashcard dto, controller

---
+++ FlashCardBuddy_API/FlashCardBuddy_API/Controllers/FlashCardController.cs
@@ -0,0 +1,79 @@
+﻿using FlashCardBuddy_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlashCardBuddy_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlashCardController : ControllerBase
+    {
+        private FlashCardBuddyDbContext dbContext = new FlashCardBuddyDbContext();
+
+        static FlashCardDTO FlashCardDTOConversion(Flashcard f)
+        {
+            return new FlashCardDTO
+
+            {
+                Question = f.Question,
+                Answer = f.Answer,
+                Stack = f.Stack,
+                Userid = f.Userid,
+
+            };
+        }
+
+        [HttpGet("allFlashCards")]
+
+        public async Task<IActionResult> GetAll(int userId)
+        {
+            List<Flashcard> result = await dbContext.Flashcards.Where(i => i.Userid == userId).ToListAsync();
+
+            if (result.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("{stack}")]
+
+        public async Task<IActionResult> GetByStack(string stack, int userId)
+        {
+            List<Flashcard> result = await dbContext.Flashcards.Where(f => f.Stack == stack && f.Userid == userId).ToListAsync();
+
+            if (result.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Add(FlashCardDTO flashcard)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Flashcard newFlashCard = new Flashcard();
+
+            newFlashCard.Question = flashcard.Question;
+            newFlashCard.Answer = flashcard.Answer;
+            newFlashCard.Stack = flashcard.Stack;
+            newFlashCard.Userid = flashcard.Userid;
+
+            dbContext.Flashcards.Add(newFlashCard);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(newFlashCard);
+        }
+
+
+    }
+}
---
+++ FlashCardBuddy_API/FlashCardBuddy_API/Models/FlashCardDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace FlashCardBuddy_API.Models
+{
+    public class FlashCardDTO
+    {
+        public string Question { get; set; } = null!;
+
+        public string Answer { get; set; } = null!;
+
+        public string? Stack { get; set; }
+
+        public int? Userid { get; set; }
+    }
+}
